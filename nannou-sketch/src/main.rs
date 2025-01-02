061fe75e82205388ccabcf2aaa0de0b47b2a3207
Bèr Kessels <ber@berk.es>
2025-01-02 15:14:33 +0100

    Feat: Change to particle tracers

--- nannou-sketch/src/main.rs
+++ nannou-sketch/src/main.rs
@@ -1,5 +1,10 @@
 extern crate nannou;
-use nannou::prelude::*;
+use nannou::{
+    noise::{NoiseFn, Perlin},
+    prelude::*,
+};
+
+const NUM_THINGS: usize = 30000;
 
 fn main() {
     nannou::app(model)
@@ -9,21 +14,73 @@ fn main() {
         .run();
 }
 
-struct Model {}
-
-fn model(_app: &App) -> Model {
-    Model {}
+struct Model {
+    things: Vec<Thing>,
+    noise: Perlin,
 }
 
-fn event(_app: &App, _model: &mut Model, _event: Event) {
+struct Thing {
+    positions: Vec<Point2>,
 }
 
-fn update(_app: &App, _model: &mut Model, _update: Update) {
+impl Thing {
+    fn new(position: Point2) -> Self {
+        Self { positions: vec![position] }
+    }
 }
 
-fn view(app: &App, _model: &Model, frame: Frame){
+fn random_things(app: &App) -> Vec<Thing> {
+    (0..NUM_THINGS)
+        .map(|_| {
+            let x = random_range(app.window_rect().left(), app.window_rect().right());
+            let y = random_range(app.window_rect().bottom(), app.window_rect().top());
+            Thing::new(pt2(x, y))
+        })
+        .collect()
+}
+fn model(app: &App) -> Model {
+    let noise = Perlin::new();
+
+    let things = random_things(app);
+    Model { things, noise }
+}
+
+fn event(_app: &App, _model: &mut Model, _event: Event) { }
+
+
+fn update(_app: &App, model: &mut Model, _update: Update) {
+    let adjust = 0.01;
+    for thing in &mut model.things {
+        let lastpos = thing.positions.last().unwrap();
+        let newpos = pt2(
+            lastpos.x + model.noise.get([
+                adjust * lastpos.x as f64,
+                adjust * lastpos.y as f64,
+                0.0,
+            ]) as f32,
+            lastpos.y + model.noise.get([
+                adjust * lastpos.x as f64,
+                adjust * lastpos.y as f64,
+                1.0,
+            ]) as f32,
+        );
+        thing.positions.push(newpos);
+    }
+}
+
+fn view(app: &App, model: &Model, frame: Frame) {
+    // let window = app.window_rect();
     let draw = app.draw();
-    draw.background().color(PINK);
-    draw.ellipse().color(LIMEGREEN);
+    if app.elapsed_frames() == 1 {
+        draw.background().color(BLACK);
+    }
+
+    for thing in &model.things {
+        draw.polyline()
+            .weight(1.0)
+            .points(thing.positions.iter().cloned())
+            .color(WHITE);
+    }
+
     draw.to_frame(app, &frame).unwrap();
 }
