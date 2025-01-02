b1d212341755ab6935242bd1a79e7f3ddd381150
Bèr Kessels <ber@berk.es>
2025-01-02 17:53:33 +0100

    feat: Make a zigzag with random, and interactive bumps
    stuck at: snapping to zigzag grid in y-axis

--- nannou-sketch/src/main.rs
+++ nannou-sketch/src/main.rs
@@ -1,10 +1,6 @@
-extern crate nannou;
-use nannou::{
-    noise::{NoiseFn, Perlin},
-    prelude::*,
-};
+use std::collections::HashMap;
 
-const NUM_THINGS: usize = 30000;
+use nannou::prelude::*;
 
 fn main() {
     nannou::app(model)
@@ -15,72 +11,97 @@ fn main() {
 }
 
 struct Model {
-    things: Vec<Thing>,
-    noise: Perlin,
-}
-
-struct Thing {
-    positions: Vec<Point2>,
-}
-
-impl Thing {
-    fn new(position: Point2) -> Self {
-        Self { positions: vec![position] }
-    }
-}
-
-fn random_things(app: &App) -> Vec<Thing> {
-    (0..NUM_THINGS)
-        .map(|_| {
-            let x = random_range(app.window_rect().left(), app.window_rect().right());
-            let y = random_range(app.window_rect().bottom(), app.window_rect().top());
-            Thing::new(pt2(x, y))
-        })
-        .collect()
+    zigs: Vec<Point2>,
+    bumps: HashMap<isize, f32>,
 }
 fn model(app: &App) -> Model {
-    let noise = Perlin::new();
+    let window = app.window_rect();
 
-    let things = random_things(app);
-    Model { things, noise }
+    // Scan from top to bottom with a zigzag pattern each 10px in hight
+    let lines = 0..window.h() as i32 / GAP as i32;
+    let left = window.left();
+    let right = window.right();
+    let top = round_to_gap(window.top());
+    let zigs = lines
+        .clone()
+        .flat_map(|i| {
+            let y_pos = top - round_to_gap(i as f32 * GAP);
+
+            // even lines: left to right one down at left
+            if i % 2 == 0 {
+                return vec![
+                    vec2(left, y_pos),
+                    // to right
+                    vec2(right, y_pos),
+                    // one line down
+                    vec2(right, y_pos - GAP),
+                ];
+            } else {
+                // odd lines: right to left one down at right
+                return vec![
+                    vec2(right, y_pos),
+                    // to left
+                    vec2(left, y_pos),
+                    // one line down
+                    vec2(left, y_pos - GAP),
+                ];
+            }
+        })
+        .collect::<Vec<_>>();
+
+    let bumps = lines
+        .map(|i| {
+            let y_pos = top - round_to_gap(i as f32 * GAP);
+            let x = random_range(left, right);
+            (y_pos as isize, x)
+        }).collect::<HashMap<_, _>>();
+
+    Model { zigs, bumps }
 }
 
-fn event(_app: &App, _model: &mut Model, _event: Event) { }
+fn round_to_gap(x: f32) -> f32 {
+    (x / GAP).round() * GAP
+}
 
+fn event(_app: &App, _model: &mut Model, _event: Event) {}
 
-fn update(_app: &App, model: &mut Model, _update: Update) {
-    let adjust = 0.01;
-    for thing in &mut model.things {
-        let lastpos = thing.positions.last().unwrap();
-        let newpos = pt2(
-            lastpos.x + model.noise.get([
-                adjust * lastpos.x as f64,
-                adjust * lastpos.y as f64,
-                0.0,
-            ]) as f32,
-            lastpos.y + model.noise.get([
-                adjust * lastpos.x as f64,
-                adjust * lastpos.y as f64,
-                1.0,
-            ]) as f32,
-        );
-        thing.positions.push(newpos);
+fn update(app: &App, model: &mut Model, _update: Update) {
+    // on a a mouse hover, add a bump to the zigzag at the mouse position
+    if app.mouse.buttons.left().is_down() {
+        let mouse = app.mouse.position();
+        // round to nearest 10.0
+        let snap = round_to_gap(mouse.y);
+        model.bumps.entry(snap as isize).and_modify(|x| *x = mouse.x).or_insert(mouse.x);
     }
 }
+
+const GAP: f32 = 10.0;
+const LINE_WIDTH: f32 = 2.0;
 
 fn view(app: &App, model: &Model, frame: Frame) {
-    // let window = app.window_rect();
     let draw = app.draw();
-    if app.elapsed_frames() == 1 {
-        draw.background().color(BLACK);
-    }
+    draw.background().color(BLACK);
 
-    for thing in &model.things {
-        draw.polyline()
-            .weight(1.0)
-            .points(thing.positions.iter().cloned())
-            .color(WHITE);
-    }
+    draw.polyline()
+        .weight(LINE_WIDTH)
+        .points(model.zigs.clone())
+        .color(WHITE);
+
+    model
+        .bumps
+        .clone()
+        .into_iter()
+        .for_each(|(y, x)| {
+            let y = y as f32;
+            let size = GAP - LINE_WIDTH;
+            let half_size = size / 2.0;
+
+            let a = vec2(x - half_size, y);
+            let b = vec2(x + half_size, y);
+            let c = vec2(x, y + size);
+            draw.polyline().weight(LINE_WIDTH).points(vec![a, b, c, a]).color(WHITE);
+            draw.polyline().weight(LINE_WIDTH).points(vec![a,b]).color(BLACK);
+        });
 
     draw.to_frame(app, &frame).unwrap();
 }
