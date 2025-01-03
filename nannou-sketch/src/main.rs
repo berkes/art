4e5f6dc78617f3682465b9b0795526cc7f44d568
Bèr Kessels <ber@berk.es>
2025-01-03 16:57:22 +0100

    feat: implement an example architecture with defaults and builder pattern

--- nannou-sketch/src/main.rs
+++ nannou-sketch/src/main.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 use nannou::prelude::*;
 
 fn main() {
@@ -10,98 +8,113 @@ fn main() {
         .run();
 }
 
+const NUM_DOTS: usize = 1000;
+
+/// Things that can be drawn on the screen.
+trait Nannou {
+    fn view(&self, draw: &Draw);
+    fn update(&mut self);
+}
+
 struct Model {
-    zigs: Vec<Point2>,
-    bumps: HashMap<isize, f32>,
-}
-fn model(app: &App) -> Model {
-    let window = app.window_rect();
-
-    // Scan from top to bottom with a zigzag pattern each 10px in hight
-    let lines = 0..window.h() as i32 / GAP as i32;
-    let left = window.left();
-    let right = window.right();
-    let top = round_to_gap(window.top());
-    let zigs = lines
-        .clone()
-        .flat_map(|i| {
-            let y_pos = top - round_to_gap(i as f32 * GAP);
-
-            // even lines: left to right one down at left
-            if i % 2 == 0 {
-                return vec![
-                    vec2(left, y_pos),
-                    // to right
-                    vec2(right, y_pos),
-                    // one line down
-                    vec2(right, y_pos - GAP),
-                ];
-            } else {
-                // odd lines: right to left one down at right
-                return vec![
-                    vec2(right, y_pos),
-                    // to left
-                    vec2(left, y_pos),
-                    // one line down
-                    vec2(left, y_pos - GAP),
-                ];
-            }
-        })
-        .collect::<Vec<_>>();
-
-    let bumps = lines
-        .map(|i| {
-            let y_pos = top - round_to_gap(i as f32 * GAP);
-            let x = random_range(left, right);
-            (y_pos as isize, x)
-        }).collect::<HashMap<_, _>>();
-
-    Model { zigs, bumps }
+    bg_color: Srgb<u8>,
+    current_bg: usize,
+    dots: Vec<Dot>,
 }
 
-fn round_to_gap(x: f32) -> f32 {
-    (x / GAP).round() * GAP
+impl Default for Model {
+    fn default() -> Self {
+        Self {
+            bg_color: HONEYDEW,
+            current_bg: usize::default(),
+            dots: Model::init_dots(),
+        }
+    }
+}
+
+impl Nannou for Model {
+    fn update(&mut self) {
+        self.dots.iter_mut().for_each(|d| d.update());
+    }
+
+    fn view(&self, draw: &Draw) {
+        draw.background().color(self.bg_color);
+        self.dots.iter().for_each(|d| d.view(draw));
+    }
+}
+
+impl Model {
+    fn init_dots() -> Vec<Dot> {
+        let mut dots = Vec::with_capacity(NUM_DOTS);
+        for _ in 0..NUM_DOTS {
+            let x = random_range(-500.0, 500.0);
+            let y = random_range(-500.0, 500.0);
+            dots.push(Dot::new(pt2(x, y)));
+        }
+        dots
+    }
+}
+
+/// A circle with a position, radius, and color.
+#[derive(Debug, Clone, Copy)]
+struct Dot {
+    color: Srgb<u8>,
+    origin: Point2,
+    radius: f32,
+    max_radius: f32,
+    growth_rate: f32,
+}
+
+impl Dot {
+    fn new(point: Point2) -> Self {
+        Self {
+            origin: point,
+            ..Default::default()
+        }
+    }
+}
+
+impl Nannou for Dot {
+    fn update(&mut self) {
+        if self.radius < self.max_radius {
+            self.radius += self.growth_rate;
+        } else {
+            self.radius = 0.0;
+        }
+    }
+
+    fn view(&self, draw: &Draw) {
+        draw.ellipse()
+            .x_y(self.origin.x, self.origin.y)
+            .radius(self.radius)
+            .color(self.color);
+    }
+}
+
+impl Default for Dot {
+    fn default() -> Self {
+        Self {
+            color: STEELBLUE,
+            origin: Point2::default(),
+            radius: 10.0,
+            max_radius: 200.0,
+            growth_rate: 1.0,
+        }
+    }
+}
+
+fn model(_app: &App) -> Model {
+    Model::default()
 }
 
 fn event(_app: &App, _model: &mut Model, _event: Event) {}
 
-fn update(app: &App, model: &mut Model, _update: Update) {
-    // on a a mouse hover, add a bump to the zigzag at the mouse position
-    if app.mouse.buttons.left().is_down() {
-        let mouse = app.mouse.position();
-        // round to nearest 10.0
-        let snap = round_to_gap(mouse.y);
-        model.bumps.entry(snap as isize).and_modify(|x| *x = mouse.x).or_insert(mouse.x);
-    }
+fn update(_app: &App, model: &mut Model, _update: Update) {
+    model.update();
 }
-
-const GAP: f32 = 10.0;
-const LINE_WIDTH: f32 = 2.0;
 
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
-    draw.background().color(BLACK);
-
-    draw.polyline()
-        .weight(LINE_WIDTH)
-        .points(model.zigs.clone())
-        .color(WHITE);
-
-    model
-        .bumps
-        .clone()
-        .into_iter()
-        .for_each(|(y, x)| {
-            let y = y as f32;
-            let size = GAP - LINE_WIDTH;
-            let half_size = size / 2.0;
-
-            let a = vec2(x - half_size, y);
-            let b = vec2(x + half_size, y);
-            let c = vec2(x, y + size);
-            draw.polyline().weight(LINE_WIDTH).points(vec![a, b, c, a]).color(WHITE);
-            draw.polyline().weight(LINE_WIDTH).points(vec![a,b]).color(BLACK);
-        });
-
+    model.view(&draw);
     draw.to_frame(app, &frame).unwrap();
 }
