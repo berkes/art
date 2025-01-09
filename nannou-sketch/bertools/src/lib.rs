b4daf0db1fd3cefb16a4ab8745b0ac823e129bdc
Bèr Kessels <ber@berk.es>
2025-01-09 18:42:15 +0100

    Feat: add util to save an image. Used to trigger with e.g. keypress

--- nannou-sketch/bertools/src/lib.rs
+++ nannou-sketch/bertools/src/lib.rs
@@ -1,7 +1,22 @@
 use nannou::{App, Draw};
+use chrono;
+
+const IMAGE_LOCATION: &str = "../saves/";
 
 /// Things that can be drawn on the screen.
 pub trait Nannou {
     fn view(&self, app: &App, draw: &Draw);
     fn update(&mut self);
+}
+
+pub fn do_save(app: &App) {
+    let now = chrono::offset::Local::now();
+
+    app.main_window().capture_frame(format!(
+            "{}{}{}{}",
+            IMAGE_LOCATION,
+            app.exe_name().unwrap(),
+            now.format("%Y-%m-%d-%H-%M-%S"),
+            ".png"
+    ));
 }
