c4cc62d541769c799ba85168aa720a86bb1b88b9
Bèr Kessels <ber@berk.es>
2025-01-21 09:01:07 +0100

    feat: add colorscheme setup in tools

--- nannou-sketch/bertools/src/lib.rs
+++ nannou-sketch/bertools/src/lib.rs
@@ -1,5 +1,5 @@
-use nannou::{App, Draw};
 use chrono;
+use nannou::{App, Draw};
 
 const IMAGE_LOCATION: &str = "../saves/";
 
@@ -13,10 +13,22 @@ pub fn do_save(app: &App) {
     let now = chrono::offset::Local::now();
 
     app.main_window().capture_frame(format!(
-            "{}{}{}{}",
-            IMAGE_LOCATION,
-            app.exe_name().unwrap(),
-            now.format("%Y-%m-%d-%H-%M-%S"),
-            ".png"
+        "{}{}{}{}",
+        IMAGE_LOCATION,
+        app.exe_name().unwrap(),
+        now.format("%Y-%m-%d-%H-%M-%S"),
+        ".png"
     ));
+}
+
+pub mod schemes {
+    use nannou::color::Hsla;
+
+    pub fn navy() -> [Hsla; 3] {
+        [
+            Hsla::new(212.0, 0.856, 0.3, 1.0), // #0B498E
+            Hsla::new(23.0, 0.25, 0.937, 1.0), // #f3eeeb
+            Hsla::new(212.0, 0.856, 0.1, 1.0), // #rgb(4 24 47)
+        ]
+    }
 }
